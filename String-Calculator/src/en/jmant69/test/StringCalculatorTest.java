c16d4ea098720a67bc1d519c90ab81bd75d69c26
Julian Mantovani <julianmantovani@gmail.com>
2024-01-24 21:00:08 +0000

    Amend to allow an unknown number of numbers in the input string

--- String-Calculator/src/en/jmant69/test/StringCalculatorTest.java
+++ String-Calculator/src/en/jmant69/test/StringCalculatorTest.java
@@ -6,27 +6,22 @@ import org.junit.Test;
 import en.jmant69.main.StringCalculator;
 
 public class StringCalculatorTest {
-
+	
 	@Test
 	public final void whenEmptyStringIsUsedThenReturnValueIs0() {
 		Assert.assertEquals(0, StringCalculator.add(""));
 	}
-
+	
 	@Test
 	public final void when1NumberIsUsedThenNoExceptionIsThrown() {
 		StringCalculator.add("1");
 	}
-
+	
 	@Test
 	public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
 		StringCalculator.add("1,2");
 	}
-
-	@Test(expected = RuntimeException.class)
-	public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-		StringCalculator.add("1,2,3");
-	}	
-
+	
 	@Test
 	public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
 		Assert.assertEquals(3, StringCalculator.add("3"));
@@ -35,6 +30,11 @@ public class StringCalculatorTest {
 	@Test
 	public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
 		Assert.assertEquals(3+6, StringCalculator.add("3,6"));
+	}
+	
+	@Test
+	public final void whenAnyNumberOfNumbersIsUsedThenReturnValueIsTheirSum() {
+		Assert.assertEquals(2+4+8+16+32, StringCalculator.add("2,4,8,16,32"));
 	}
 
 }
